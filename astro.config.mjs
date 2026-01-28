169767c934da1a7a97b90f781446acff110553d2
drntoday <drn.today@outlook.com>
2026-01-28 12:59:58 +0530

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://yourusername.github.io/drn-today',
+  base: '/drn-today',
+  integrations: [],
+  output: 'static'
+});
